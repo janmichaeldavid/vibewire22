07ee82edb291f05a8d46e5d2e70154a579f1313b
JanMichael Guzman <janmichaelguzman@gmail.com>
2022-04-21 09:49:49 -0400

    end of section 1

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import './app/layout/styles.css';
+import App from './app/layout/App';
 import reportWebVitals from './reportWebVitals';
+import { render } from '@testing-library/react';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -10,6 +11,14 @@ root.render(
     <App />
   </React.StrictMode>
 );
+
+if (module.hot) {
+  module.hot.accept('./app/layout/App', function(){
+    setTimeout(render);
+  })
+}
+
+render();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
